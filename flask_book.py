8805099c1eb54f0eaca87421a9a9115777ee2faf
Kenneth Reitz <me@kennethreitz.com>
2012-03-01 16:53:55 -0500

    test

--- flask_book.py
+++ flask_book.py
@@ -2,26 +2,27 @@ from __future__ import absolute_import
 
 from flask import _request_ctx_stack
 
+
 class Facebook(object):
 
     def __init__(self, app):
         self.app = app
-        self.app.config.setdefault('SQLITE3_DATABASE', ':memory:')
-        self.app.teardown_request(self.teardown_request)
-        self.app.before_request(self.before_request)
+        self.app.config.setdefault('FACEBOOK_APP_ID', None)
+        self.app.config.setdefault('FACEBOOK_SECRET', None)
 
-    def connect(self):
-        return sqlite3.connect(self.app.config['SQLITE3_DATABASE'])
 
-    def before_request(self):
-        ctx = _request_ctx_stack.top
-        ctx.sqlite3_db = self.connect()
 
-    def teardown_request(self, exception):
-        ctx = _request_ctx_stack.top
-        ctx.sqlite3_db.close()
+    # def before_request(self):
+    #     ctx = _request_ctx_stack.top
+    #     ctx.sqlite3_db = self.connect()
 
-    def get_db(self):
-        ctx = _request_ctx_stack.top
-        if ctx is not None:
-            return ctx.sqlite3_db
+    # def teardown_request(self, exception):
+    #     ctx = _request_ctx_stack.top
+    #     ctx.sqlite3_db.close()
+
+    # def get_db(self):
+    #     ctx = _request_ctx_stack.top
+    #     if ctx is not None:
+    #         return ctx.sqlite3_db
+
+
